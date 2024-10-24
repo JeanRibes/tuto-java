67574537f3f3e0fe288f0f43f007038bdb1d5dfe
Jean Ribes <jean.ribes@polyconseil.fr>
2024-10-24 10:41:13 +0200

    fix javanaise

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 ## Tu as trouvé, explique pourquoi ça ne peut pas fonctionner pas besoin de me corriger.
 ## tu peux supprimer le deploiement de cette image une fois que tu as expliqué à l'examinateur la raison de l'erreur.
-sed -i 's/localhost/postgres-service/g' application.yml && java -jar app-java-forge-demo.jar
+cat application.yml | sed  's/localhost/postgres-service/g' > /tmp/application.yml 
+cat /tmp/application.yml > application.yml
+java -jar app-java-forge-demo.jar
